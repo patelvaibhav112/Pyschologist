acaf4d6680ad40a4d915915ac346d001ab1f6892
Vaibhav Patel <patel.vaibhav@gmail.com>
2012-10-25 10:17:18 -0700

    Segue support

--- Psychologist/PsychologistViewController.m
+++ Psychologist/PsychologistViewController.m
@@ -26,11 +26,17 @@
 {
     if([segue.identifier isEqualToString:@"ShowDiagnosis"]){
         [segue.destinationViewController setHappiness:self.diagnosis];
+    } else if([segue.identifier isEqualToString:@"celebritySegue"]){
+        [segue.destinationViewController setHappiness:100];
+    } else if([segue.identifier isEqualToString:@"problemSegue"]){
+        [segue.destinationViewController setHappiness:20];
+    } else if([segue.identifier isEqualToString:@"tvKookSegue"]){
+        [segue.destinationViewController setHappiness:50];
     }
 }
 - (IBAction)flying
 {
-    [self setAndShowDiagnosis:85];
+    [self setAndShowDiagnosis:50];
 }
 
 
